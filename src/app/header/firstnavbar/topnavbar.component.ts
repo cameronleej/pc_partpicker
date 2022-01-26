84c6bf59388b3df8b0c11c62c61ac7ba9bf11a1e
Cameron <clj00004@mix.wvu.edu>
2022-01-25 21:56:16 -0500

    Finished TopNav

---
+++ src/app/header/firstnavbar/topnavbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'pcpart-topnavbar',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+
+
+export class TopNavBarComponent{
+
+
+}
