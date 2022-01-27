20a2107a9495d06447cc084cbaafa580fa89670f
Cameron <clj00004@mix.wvu.edu>
2022-01-26 19:12:54 -0500

    Second Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/firstnavbar/topnavbar.component';
+import { SecondNavBarComponent } from './header/secondnavbar/secondnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
-  ],
+    TopNavBarComponent,
+    SecondNavBarComponent
+    ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/header/secondnavbar/secondnavbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'pcpart-secondnavbar',
+    templateUrl: 'secondnavbar.component.html',
+    styleUrls: ['secondnavbar.component.css']
+})
+
+
+export class SecondNavBarComponent{
+
+
+}
